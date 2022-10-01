d6481079f15db35b9abf31fa2b11eb334a458340
diomededavid <david@d2creativedesign.com>
2022-09-30 20:53:33 -0700

    moves origial storybook compoenents to components folder

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Header } from './Header';
+import { Header } from '../Header/Header';
 import './page.css';
 
 type User = {
@@ -45,7 +45,7 @@ export const Page: React.VFC = () => {
         </ul>
         <p>
           Get a guided tutorial on component-driven development at{' '}
-          <a href="https://storybook.js.org/tutorials/" target="_blank" rel="noopener noreferrer">
+          <a href="src/components/Page/Page" target="_blank" rel="noopener noreferrer">
             Storybook tutorials
           </a>
           . Read more in the{' '}
