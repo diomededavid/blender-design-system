ffe163bc5710d8098ae3ee95df81d01633c59c1a
David Diomede <david@d2creativedesign.com>
2022-12-29 10:09:09 -0700

    moves html content into story and updates button props

--- src/components/Hero/index.stories.tsx
+++ src/components/Hero/index.stories.tsx
@@ -1,20 +1,30 @@
-import {ComponentStory, ComponentMeta} from "@storybook/react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 import { Hero } from "./index";
+import Button from "../Button";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-    title: "Atoms/Hero",
-    component: Hero,
+  title: "Atoms/Hero",
+  component: Hero,
 } as ComponentMeta<typeof Hero>;
 
 const Template: ComponentStory<typeof Hero> = (args) => <Hero {...args} />;
 
-export const Default = () => <Hero>
+export const Default = () => (
+  <Hero>
+    <img
+      src="https://placeimg.com/260/400/arch"
+      className="max-w-sm rounded-lg shadow-2xl"
+      alt="placeholder"
+    />
     <div>
-    <h1 className="text-5xl font-bold">Box Office News!</h1>
-    <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-    <button className="btn btn-primary">Get Started</button>
-</div></Hero>;
-
-
-
+      <h1 className="text-5xl font-bold">Box Office News!</h1>
+      <p className="py-6">
+        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+        excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a
+        id nisi.
+      </p>
+      <Button color="primary" label="Get Started" />
+    </div>
+  </Hero>
+);
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,20 +1,16 @@
-interface HeroProps {
-    /**
-     * Accept children components
-     */
+import React from "react";
 
-     children?: React.ReactNode;
+interface HeroProps {
+  /**
+   * Accept children components
+   */
+  children?: React.ReactNode;
 }
 
-export const Hero = ({children}: HeroProps) => (
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-            <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" />
-            <div>
-             {children}
-            </div>
-        </div>
-    </div>
-)
+export const Hero = ({ children }: HeroProps) => (
+  <div className="hero min-h-screen bg-base-200">
+    <div className="hero-content flex-col lg:flex-row">{children}</div>
+  </div>
+);
 
 export default Hero;
