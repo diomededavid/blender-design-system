b0789a8f3325698321aa1c16ac38dddfd095fd2d
David Diomede <david@d2creativedesign.com>
2022-12-29 10:04:10 -0700

    updates button props on card

--- src/components/Card/index.stories.tsx
+++ src/components/Card/index.stories.tsx
@@ -1,6 +1,6 @@
-import {ComponentStory, ComponentMeta} from "@storybook/react";
-import {Card} from "./index";
-import {Button} from "../Button/";
+import { ComponentMeta } from "@storybook/react";
+import { Card } from "./index";
+import { Button } from "../Button/";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
@@ -8,15 +8,13 @@ export default {
   component: Card,
 } as ComponentMeta<typeof Card>;
 
-const Template: ComponentStory<typeof Card> = (args) => <Card {...args} />;
-
 export const Default = () => <Card />;
 
 export const CardActions = () => (
   <Card>
-      <h2>Heading</h2>
-      <h2 className="card-title">Card title!</h2>
-      <p>If a dog chews shoes whose shoes does he choose?</p>
-    <Button label="label" color="btn-primary" />
+    <h2>Heading</h2>
+    <h2 className="card-title">Card title!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <Button label="label" color="primary" />
   </Card>
 );
