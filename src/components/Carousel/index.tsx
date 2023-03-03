6e089be10dbf4ae6fa0c536bc9ca9b26bd0fa565
joshuaDclark <mrjoshuadclark@gmail.com>
2023-03-02 20:51:13 -0500

    added arrows to carousel

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -39,7 +39,15 @@ interface ButtonProps {
  */
 export const Carousel = () => {
   return (
-    <div className="carousel rounded-box">
+    <div className="relative carousel rounded-box">
+      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a href="#slide4" className="btn btn-circle">
+          ❮
+        </a>
+        <a href="#slide2" className="btn btn-circle">
+          ❯
+        </a>
+      </div>
       <div className="carousel-item">
         <img
           src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg"
